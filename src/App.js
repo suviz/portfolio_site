6cfe2a81316795fce0aa2d38e5c07b36b0646268
suviz <suvi.zitting@gmail.com>
2024-05-04 14:27:04 +0300

    Add site structure, improve background

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import NavBar from './components/navbar';
 
 function App() {
   return (
-    <div className="App ">
+    <div className="App background">
 
-      <header className="background" >
+      <header className="" >
         <NavBar/>
       </header>
 
+      <main>
+
+        <div className='hero'>
+          <h1> 
+            Moikkuli!
+          </h1>
+
+          <p>Description</p>
+
+          <div className='buttons'>
+            <p>Buttons</p>
+          </div>
+        </div>
+
+  
+        <div className='projects'>
+          <h2>Projects</h2>
+        </div>
+
+        <div className='contact'>
+          <h2>Contact me</h2>
+        </div>
+
+      </main>
+
+    
     </div>
   );
 }

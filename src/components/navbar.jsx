9449984a0563ac17484d599a55da80ffd08745eb
suviz <suvi.zitting@gmail.com>
2024-05-07 16:36:17 +0300

    Remove email from navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import './navbar.css';
 import github from '../pictures/github.png';
 import linkedin from '../pictures/linkedin.png';
-import email from '../pictures/email.png';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
@@ -11,16 +10,12 @@ export default function NavBar() {
 
       <div className='icons'>
 
-        <Link to="//www.github.com/suviz" className='icon-a'> 
+        <Link to="//www.github.com/suviz" className='icon-link'> 
           <img src={github} alt="github" className='icon'></img>
         </Link>
 
-        <Link to="//www.linkedin.com/in/suvi-zitting-84719723a" className='icon-a'>
+        <Link to="//www.linkedin.com/in/suvi-zitting-84719723a" className='icon-link'>
           <img src={linkedin} alt="linkedin" className='icon'></img>
-        </Link>
-
-        <Link to="/#contact" className='icon-a'>
-          <img src={email} alt="email" className='icon'></img>
         </Link>
 
       </div>

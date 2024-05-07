bd185c7adff9827adcccf61911c80b0f89876b90
suviz <suvi.zitting@gmail.com>
2024-05-07 16:32:02 +0300

    Implement smooth scrolling to buttons

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,33 +1,42 @@
 import { Link } from "react-router-dom";
+import { useRef } from 'react';
 
 function HomePage() {
+
+    const projectRef = useRef();
+    const contactRef = useRef();
+
     return (
   
         <main>
-  
-          <div className='hero'>
-            <h1> 
-              Moikkuli!
-            </h1>
-  
-            <p className='hero-desc'>Description</p>
-  
-            <div className='buttons'>
-                <Link to="/about" className='button'>About me</Link>
-                <Link to="#projects" className='button'>Projects</Link>
-                <Link to="#contact" className='button'>Contact</Link>
-            </div>
-          </div>
-  
-    
-          <div className='projects' id="projects">
-            <h2>Projects</h2>
-          </div>
-  
-          <div className='contact' id="contact">
-            <h2>Contact me</h2>
-          </div>
-  
+            
+            <section id="hero">
+                <div className='hero'>
+
+                    <h1>Moikkuli!</h1>
+                    <p className='hero-desc'>Description</p>
+        
+                    <div className='buttons'>
+                        <Link to="/about" className='button'>About me</Link>
+                        <Link to="#projects" className='button' onClick={() => projectRef.current?.scrollIntoView({behavior: 'smooth'})}>Projects</Link>
+                        <Link to="#contact" className='button' onClick={() => contactRef.current?.scrollIntoView({behavior: 'smooth'})}>Contact</Link>
+                    </div>
+                    
+                </div>
+            </section>
+            
+            <section ref={projectRef} id="projects">
+                <div className='projects' >
+                    <h2>Projects</h2>
+                </div>
+            </section>
+            
+            <section ref={contactRef} id="contact">
+                <div className='contact'>
+                    <h2>Contact me</h2>
+                </div>
+            </section>
+          
         </main>
   
     );

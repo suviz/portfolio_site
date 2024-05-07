dbf74becef375f1faffe6d17920f081d3154ee91
suviz <suvi.zitting@gmail.com>
2024-05-07 17:16:49 +0300

    Add name to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,25 +7,34 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div className='navbar'>
+      
+      <div className='logo'>
+        Suvi Zitting
+      </div>
 
       <div className='links'>
-        <Link to="/" className='nav-link'>Home </Link>
-        <Link to="/about" className='nav-link'>About me</Link>
+
+        <div className='nav-links'>
+          <Link to="/" className='nav-link'>Home </Link>
+          <Link to="/about" className='nav-link'>About me</Link>
+        </div>
+
+        <span className='separator'></span>
+
+        <div className='icons'>
+
+          <Link to="//www.github.com/suviz" className='icon-link'> 
+            <img src={github} alt="github" className='icon'></img>
+          </Link>
+
+          <Link to="//www.linkedin.com/in/suvi-zitting-84719723a" className='icon-link'>
+            <img src={linkedin} alt="linkedin" className='icon'></img>
+          </Link>
+
+        </div>
+
       </div>
-
-      <span className='separator'></span>
-
-      <div className='icons'>
-
-        <Link to="//www.github.com/suviz" className='icon-link'> 
-          <img src={github} alt="github" className='icon'></img>
-        </Link>
-
-        <Link to="//www.linkedin.com/in/suvi-zitting-84719723a" className='icon-link'>
-          <img src={linkedin} alt="linkedin" className='icon'></img>
-        </Link>
-
-      </div>
+      
 
       
     </div>

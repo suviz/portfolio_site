083ab9e5c1fc3d2bdf228124e4e157477626fdf3
suviz <suvi.zitting@gmail.com>
2024-05-09 11:10:04 +0300

    Add responsivity to page layouts

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -5,10 +5,8 @@ function AboutPage() {
         <main>
   
           <div className='hero'>
-            <h1> 
-              About me
-            </h1>
-  
+            
+            <h2>About me</h2>
             <p className='hero-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   
           </div>

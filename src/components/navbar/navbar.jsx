55a7c8a4fb9a40f7e59241e4bd77779754834315
suviz <suvi.zitting@gmail.com>
2024-05-12 15:16:14 +0300

    Make current page show in navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import './navbar.css';
+import { Link, useLocation } from 'react-router-dom'
 import github from '../../pictures/github.png';
 import linkedin from '../../pictures/linkedin.png';
-import { Link } from 'react-router-dom';
+import './navbar.css';
 
 export default function NavBar() {
+  const url = useLocation().pathname;
+  
   return (
     <div className='navbar'>
       
@@ -15,8 +17,8 @@ export default function NavBar() {
       <div className='nav-text-links'>
 
         <div className='nav-textlinks'>
-          <Link to="/" className='nav-textlink'>Home </Link>
-          <Link to="/about" className='nav-textlink'>About</Link>
+          <Link to="/" className={`nav-textlink ${(url === '/') ? "active" : ""}`} >Home </Link>
+          <Link to="/about" className={`nav-textlink ${(url === '/about') ? "active" : ""}`}>About</Link>
         </div>
 
         <span className='nav-separator'></span>
@@ -32,8 +34,11 @@ export default function NavBar() {
           </Link>
 
         </div>
+        
 
       </div>
+
+      
 
     </div>
   );
